c7bc19ea8062a016a8d0a198fb251b76eabd748f
Farzad Dehghani <frzd.dh@outlook.com>
2022-10-10 14:20:11 +0330

    change in Scramble

--- src/Owleye.Scramble/Startup.cs
+++ src/Owleye.Scramble/Startup.cs
@@ -1,20 +1,8 @@
-using EasyCaching.Core.Configurations;
-using LiteX.Email;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Owleye.Application.Handlers;
-using Owleye.Application.Sensors.Queries.GetSensorsList;
-using Owleye.Infrastructure.Cache;
-using Owleye.Infrastructure.Data;
-using Owleye.Infrastructure.MappingConfiguration;
-using Owleye.Shared.Base;
-using Owleye.Shared.Cache;
-using Owleye.Shared.Data;
 
 namespace Owleye.Scramble
 {
@@ -35,27 +23,7 @@ namespace Owleye.Scramble
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
                 .AddMvcOptions(option => option.EnableEndpointRouting = false);
 
-            services.AddTransient<IRedisCache, RedisCache>();
-            services.AddMediatR(typeof(DoPingNotificationHandler).Assembly);
-            services.AddMediatR(typeof(GetSensorsListQueryHandler).Assembly);
             services.AddOptions();
-            services.AddEasyCaching(options =>
-            {
-                options.UseRedis(config =>
-                {
-                    config.DBConfig.Endpoints.Add(new
-                        ServerEndPoint(Configuration["General:RedisAddress"],
-                        int.Parse(Configuration["General:RedisPort"])));
-                }, Configuration["General:RedisInstanceName"]) //redis provider name
-                .WithMessagePack()
-                .UseRedisLock(); //with distributed lock, prevent problem in aysnc.
-            });
-            services.AddLiteXSmtpEmail();
-            services.AddScoped<IAppSession, AppSession>();
-            services.AddDbContext<OwleyeDbContext>(options =>
-               options.UseSqlServer(Configuration.GetConnectionString(nameof(OwleyeDbContext))), ServiceLifetime.Transient);
-            services.AddAutoMapper(typeof(SensorAutoMapProfile));
-            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,15 +39,11 @@ namespace Owleye.Scramble
             }
 
             app.UseStaticFiles();
-
-          
             app.UseAuthorization();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
-
 
             app.UseMvc(routes =>
             {
--- src/Owleye.Shared/Util/WebSiteUtil.cs
+++ src/Owleye.Shared/Util/WebSiteUtil.cs
@@ -5,7 +5,7 @@ namespace Owleye.Shared.Util
 {
     public static class WebSiteUtil
     {
-        public static bool IsUrlAlive(string url,int urlTimeout)
+        public static bool IsUrlAlive(string url, int urlTimeout)
         {
             try
             {
@@ -39,6 +39,38 @@ namespace Owleye.Shared.Util
                 //TODO, log this.
             }
             return false;
+        }
+
+        public static Tuple<bool, int> IsUrlAliveWithStatus(string url, int urlTimeout)
+        {
+            try
+            {
+                var request = WebRequest.Create(url) as HttpWebRequest;
+                request.Timeout = urlTimeout;
+                request.Method = "HEAD"; // TODO fix this, head maybe disallowed in some situations
+
+                if (request.GetResponse() is HttpWebResponse response)
+                {
+                    var statusCode = (int)response.StatusCode;
+                    if (statusCode >= 100 && statusCode < 400)
+                    {
+                        return Tuple.Create(true, statusCode);
+                    }
+
+                    return Tuple.Create(false, statusCode);
+
+                }
+            }
+            catch (WebException ex)
+            {
+                var webResponse = ex.Response as HttpWebResponse;
+                return Tuple.Create(false, (int)webResponse.StatusCode);
+            }
+            catch (Exception)
+            {
+                //TODO, log this.
+            }
+            return Tuple.Create(false, 500);
         }
 
     }
